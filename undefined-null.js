e366811ed787ec22a7aeccee59c72e902cfb36b4
arafat356 <ahmedarafatft@gmail.com>
2022-02-06 03:28:27 +0600

    nothing

---
+++ undefined-null.js
@@ -0,0 +1,19 @@
+/*
+
+*/
+let first;
+// console.log(first);
+function second (x,y){
+         const sum = x+y;
+}
+const result = second(3,43);
+console.log(result);
+function duble (a,b){
+         const result = a*2;
+         console.log(b)
+         return result;
+}
+const fifth = {name: 'kamal',age:15,location:'khiram'}
+console.log(fifth.name)
+const sixth = [33,44,55,66,77,88,99,11,22,33,44,];
+console.log(sixth[5])
